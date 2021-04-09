fc471a55f4680c84129b2fdf95c1a55a5c49e043
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-04-09 18:45:34 +0300

    Add node enviorment

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const { PORT } = process.env || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${process.env.NODE_ENV} port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
 });
